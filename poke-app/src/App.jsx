8585cf48b7d0ba8b30b3746baea57f8b9717eb00
Ferresmash <120678062+Ferresmash@users.noreply.github.com>
2025-04-24 15:12:15 +0200

    fixed layout so it overflows nicely

--- poke-app/src/App.jsx
+++ poke-app/src/App.jsx
@@ -5,7 +5,6 @@ import PokeList from './PokeList';
 import DetailsPanel from './DetailsPanel';
 
 function App() {
-    // Current Pokémon is both the latest searched Pokémon and the selected Pokémon
     const [currPokemon, setCurrPokemon] = useState(null);
     const [pokeStorage, setPokeStorage] = useState([]);
 
@@ -29,17 +28,22 @@ function App() {
 
     return (
         <>
-            <div className="flex flex-col bg-base-300 items-center  h-full w-full">
-                <div className="container">
-                    <Header onPokemonSearch={handlePokemonSearch} />
-                    <div className="flex flex-row bg-base-100 m-2 justify-between rounded-lg w-full">
-                        <PokeList
-                            onPokemonSelect={setCurrPokemon} pokemons={pokeStorage} onPokemonRemove={removePokemon}
-                        /> 
-                        <DetailsPanel pokemon={currPokemon}/>
-                    </div>
+            <div className="flex flex-col bg-base-300 items-center w-full h-screen">
+            <div className="container flex flex-col h-full">
+                {/* Header */}
+                <Header onPokemonSearch={handlePokemonSearch} />
+
+                {/* Main Content */}
+                <div className="flex flex-row bg-base-100 m-2 justify-between rounded-lg w-full flex-grow overflow-auto">
+                    <PokeList
+                        onPokemonSelect={setCurrPokemon}
+                        pokemons={pokeStorage}
+                        onPokemonRemove={removePokemon}
+                    />
+                    <DetailsPanel pokemon={currPokemon} />
                 </div>
             </div>
+        </div>
         </>
     );
 }
